c6356e2310e8aaa0d57150b76863099b55bf11dd
RobinTheRedLight <robinmitra789@gmail.com>
2024-07-10 17:28:12 +0600

    product-route-completed

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config();
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-res.send('Server is running')
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fghxosh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-app.listen(port, ()=> {
-console.log(`Server is running on port ${port}`)
-})
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const productCollection = client.db("SportSpot").collection("products");
+
+    app.get("/products", async (req, res) => {
+      const result = await productCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
