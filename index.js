69e4b3b5907e184fec845cbc410f205e62f220a3
RobinTheRedLight <robinmitra789@gmail.com>
2024-07-11 16:01:13 +0600

    singleProduct-route-completed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 //middleware
 app.use(cors());
@@ -26,8 +26,23 @@ async function run() {
     const productCollection = client.db("SportSpot").collection("products");
 
     app.get("/products", async (req, res) => {
+      // if (id !== undefined) {
+      //   const id = req.params.id;
+      //   const ObjectID = require("mongodb").ObjectId;
+      //   const query = { _id: ObjectId(id) };
+      //   const product = await productCollection.findOne(query);
+      //   res.send(product);
+      // } else {
       const result = await productCollection.find().toArray();
       res.send(result);
+      // }
+    });
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const product = await productCollection.findOne(filter);
+      res.send(product);
     });
 
     // Send a ping to confirm a successful connection
